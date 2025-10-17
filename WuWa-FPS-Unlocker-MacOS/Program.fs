d57e0850bf601437437cb2dfd7d4f99b151d10b4
Peekaey <dallasjt31@gmail.com>
2025-10-17 22:52:27 +1100

    Initial Commit

--- WuWa-FPS-Unlocker-MacOS/Program.fs
+++ WuWa-FPS-Unlocker-MacOS/Program.fs
@@ -1,2 +1,23 @@
 ﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+
+
+open System
+open System.IO
+
+[<EntryPoint>]
+printfn "Starting Uncapping of FPS for Wuthering Waves..."
+printfn "Please note that this utility was only designed for operating on MacOS and not windows"
+
+// First we identifiy if the LocalStorage.db exists or not in the users device
+let homeDir = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile)
+let localStoragePath = $"{homeDir}/Library/Containers/com.kurogame.wutheringwaves.global/Data/Library/Client/Saved/LocalStorage/LocalStorage.db"
+
+if not (File.Exists(localStoragePath)) then
+    printfn("LocalStorage file not found")
+    exit 1
+else
+    printfn("LocalStorage file was found")
+    
+    
+    
+        
